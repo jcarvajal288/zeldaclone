871c116308a809b8a661a607e612cd120bc24f7a
Max Carvajal <jcarvajal288@gmail.com>
2025-07-02 10:48:51 -0500

    Abstract out camera movement

--- src/camera.gd
+++ src/camera.gd
@@ -1,7 +1,20 @@
 extends Camera2D
 
+var MAP_CELL_SIZE = Vector2(160, 120)
+enum DIRECTION {UP, DOWN, LEFT, RIGHT}
+
+func _move_camera(direction):
+	if direction == DIRECTION.UP:
+		self.position.y -= MAP_CELL_SIZE.y
+	elif direction == DIRECTION.DOWN:
+		self.position.y += MAP_CELL_SIZE.y
+	elif direction == DIRECTION.RIGHT:
+		self.position.x += MAP_CELL_SIZE.x
+	elif direction == DIRECTION.LEFT:
+		self.position.x -= MAP_CELL_SIZE.x
+
 func _process(delta: float):
 	if Input.is_action_just_pressed("ui_home"):
-		self.position = Vector2(0, -120)
+		_move_camera(DIRECTION.UP)
 	elif Input.is_action_just_pressed("ui_end"):
-		self.position = Vector2(0, 0)
+		_move_camera(DIRECTION.DOWN)
