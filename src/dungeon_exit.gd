ee23775524bdfac5cbf91eda37e3e1b6cd401e16
Max Carvajal <jcarvajal288@gmail.com>
2025-07-03 15:32:02 -0500

    Transition out of dungeon back to grasslands

---
+++ src/dungeon_exit.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	Global.level_manager.change_to_grasslands()
--- src/level_manager.gd
+++ src/level_manager.gd
@@ -7,7 +7,8 @@ var current_level
 
 func _ready():
 	Global.level_manager = self
-	_change_level(grasslands)
+	#_change_level(grasslands)
+	_change_level(dungeon)
 	
 func _change_level(packed_new_level: PackedScene, delete: bool = true, keep_running: bool = false):
 	if current_level != null:
@@ -23,4 +24,8 @@ func _change_level(packed_new_level: PackedScene, delete: bool = true, keep_runn
 
 func change_to_dungeon():
 	_change_level(dungeon)
-	Global.game_controller.move_player_to_position(Vector2(84, 24))
+	Global.game_controller.move_player_to_position(Vector2(80, 20))
+
+func change_to_grasslands():
+	_change_level(grasslands)
+	Global.game_controller.move_player_to_position(Vector2(231, -56))
