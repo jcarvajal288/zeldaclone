8348c0921e6a1a302174a0152073d27a2e54f81b
Max Carvajal <jcarvajal288@gmail.com>
2025-07-06 17:17:44 -0500

    Orcs take damage and die at 0 hp

--- Entities/Enemies/Orc/orc.gd
+++ Entities/Enemies/Orc/orc.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var facing = "DownRight"
+var max_health = 3
+var current_health = max_health
 
 func _ready():
 	var random_idle = Global.rng.randi_range(1, 4)
@@ -19,8 +21,12 @@ func _ready():
 	var random_time = Global.rng.randf_range(0, $AnimationPlayer.current_animation_length)
 	$AnimationPlayer.seek(random_time)
 
-func _on_hurtbox_area_entered(area: Hitbox) -> void:
-	$AnimationPlayer.play("damaged" + facing)
+func _on_hurtbox_area_entered(hitbox: Hitbox) -> void:
+	current_health -= hitbox.damage
+	if current_health > 0:
+		$AnimationPlayer.play("damaged" + facing)
+	else:
+		$AnimationPlayer.play("Death")
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
--- Utilities/hitbox.gd
+++ Utilities/hitbox.gd
@@ -1,1 +1,3 @@
 class_name Hitbox extends Area2D
+
+@export var damage: int
