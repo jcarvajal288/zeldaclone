d8fdc2c77a9464484972b2b9dba133515d60bf4d
Max Carvajal <jcarvajal288@gmail.com>
2025-07-18 15:23:33 -0500

    Add Dungeon 2, increase map cell size

--- src/global.gd
+++ src/global.gd
@@ -5,7 +5,7 @@ var level_manager: LevelManager
 var rng: RandomNumberGenerator
 
 var MAP_TILE_SIZE = 8
-var MAP_CELL_SIZE = Vector2(MAP_TILE_SIZE * 20, MAP_TILE_SIZE * 15)
+var MAP_CELL_SIZE = Vector2(MAP_TILE_SIZE * 21, MAP_TILE_SIZE * 15)
 var PLAYER_POSITION = Vector2(0, 0)
 
 enum Item {
