d548b7bd1927563cfe871ed2b60e6a1bf78e8241
Max Carvajal <jcarvajal288@gmail.com>
2025-07-23 10:42:46 -0500

    Corrected pit fall animation

--- Entities/Characters/character.gd
+++ Entities/Characters/character.gd
@@ -11,8 +11,6 @@ func take_pit_damage():
 	$Health.take_damage(1)
 
 
-func fall_down_pit(fall_point: Vector2):
-	$AnimationPlayer.animation_locked = true
+func fall_down_pit(fall_direction: Vector2):
 	$AnimationPlayer.play_fall_animation()
-	var fall_direction = (self.position -fall_point).normalized()
 	self.velocity = fall_direction * 20
--- Levels/Components/Pit/pit.gd
+++ Levels/Components/Pit/pit.gd
@@ -1,9 +1,7 @@
 class_name Pit extends Area2D
 
-@export var respawn_points: Array[Area2D]
 
 func _on_body_entered(character: Character) -> void:
-	print("fell into pit body")
 	character.take_pit_damage()
-	var center_point = $PitCollisionShape.shape.get_rect().get_center()
-	character.fall_down_pit(center_point)
+	var fall_direction = ($PitCollisionShape.position - character.position).normalized()
+	character.fall_down_pit(fall_direction)
