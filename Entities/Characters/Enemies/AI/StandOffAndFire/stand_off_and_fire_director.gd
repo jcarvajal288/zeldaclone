4e67ecf6e413f56dd49d2cd2a9ea6a3cb250230f
Max Carvajal <jcarvajal288@gmail.com>
2025-08-24 15:37:55 -0500

    Stone flies correctly and disappears on impact

--- Entities/Characters/Enemies/AI/StandOffAndFire/stand_off_and_fire_director.gd
+++ Entities/Characters/Enemies/AI/StandOffAndFire/stand_off_and_fire_director.gd
@@ -9,19 +9,14 @@ extends Director
 func _process(_delta: float) -> void:
 	reset()
 	if not can_see_player():
-		# print("halting")
 		halt()
 	elif player_beyond_range(ranged_attack_range_pixels):
-		# print("moving towards")
 		move_towards_player()
 	elif player_beyond_range(run_away_range):
-		# print("firing")
 		fire_ranged()
 	elif player_beyond_range(melee_attack_range_pixels):
-		# print("moving away")
 		move_away_from_player()
 	else:
-		# print("attacking")
 		attack_player()
 	subject.move_and_slide()
 
--- Entities/Characters/Enemies/Otter/Stone/stone.gd
+++ Entities/Characters/Enemies/Otter/Stone/stone.gd
@@ -1,14 +1,20 @@
 class_name Stone extends RigidBody2D
 
-@onready var stone_speed = 20
+@onready var stone_speed = 50
 
 
 func _ready() -> void:
 	linear_damp = 0.0
 	gravity_scale = 0.0
+	contact_monitor = true
+	max_contacts_reported = 1
+	body_entered.connect(on_body_entered)
 
 
 func set_direction(direction: Vector2) -> void:
 	apply_central_impulse(direction * stone_speed)
-	rotation = direction.angle() + 90.0
+	rotation = direction.angle() + PI / 2
 
+
+func on_body_entered(_body: Node2D) -> void:
+	queue_free()
